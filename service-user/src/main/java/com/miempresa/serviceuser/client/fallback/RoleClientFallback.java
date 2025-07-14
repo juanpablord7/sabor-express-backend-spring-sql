2f918993cc70dfa5c3a792d0206b5a8c4b309466
Juan Pablo <juan.roman.privado@gmail.com>
2025-07-14 00:03:49 -0500

    v1.0.0

--- service-user/src/main/java/com/miempresa/serviceuser/client/fallback/RoleClientFallback.java
+++ service-user/src/main/java/com/miempresa/serviceuser/client/fallback/RoleClientFallback.java
@@ -1,18 +1,4 @@
 package com.miempresa.serviceuser.client.fallback;
 
-import com.miempresa.serviceuser.client.RoleClient;
-import com.miempresa.serviceuser.dto.client.RoleResponse;
-import org.springframework.stereotype.Component;
-
-@Component
-public class RoleClientFallback implements RoleClient {
-    @Override
-    public Long getDefaultRole(){
-        return null;
-    }
-
-    @Override
-    public RoleResponse getRole(Long id) {
-        return RoleResponse.builder().build();
-    }
+public class RoleClientFallback {
 }
---
+++ service-user/src/main/java/com/miempresa/serviceuser/dto/user/UserView.java
@@ -0,0 +1,4 @@
+package com.miempresa.serviceuser.dto.user;
+
+public interface UserResponse {
+}
